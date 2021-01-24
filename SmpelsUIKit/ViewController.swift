71569d3f7414c3447373f3c28b342753f1e621d0
Tal <talspektor.gmail.com>
2021-01-24 08:47:52 +0200

    Initial Commit

---
+++ SmpelsUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmpelsUIKit
+//
+//  Created by Tal Spektor on 24/01/2021.
+//  Copyright © 2021 Tal Spektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
